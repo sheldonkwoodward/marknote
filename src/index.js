f71cd657c6b2e1349f3a216c12a3f5763dc3d62e
Sheldon Woodward <sheldon.woodward@ahss.org>
2018-08-10 10:57:25 -0400

    Initialized project.

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './components/App/App.css';
+import App from './components/App/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
