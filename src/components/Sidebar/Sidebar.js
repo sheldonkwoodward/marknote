6b8e8d465c0e145293a2b47da4b74b954b6bc2b9
Sheldon Woodward <sheldon.woodward@ahss.org>
2018-08-10 11:23:30 -0400

    Updated Sidebar component. Added bootstrap styles to index.html.

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import './Sidebar.css';
 
+import SidebarItem from '../SidebarItem/SidebarItem';
+
 
 class Sidebar extends Component {
   render() {
     return (
       <div className="Sidebar">
         <h1>Sidebar</h1>
+        <ol>
+          <li>
+            <SidebarItem />
+          </li>
+          <li>
+            <SidebarItem />
+          </li>
+          <li>
+            <SidebarItem />
+          </li>
+        </ol>
       </div>
     );
   }
