31ea75c6481658646c0c7c96b27328e45d482699
Sheldon Woodward <sheldon.woodward@ahss.org>
2018-08-10 11:22:36 -0400

    Added sidebar component.

---
+++ src/components/SidebarItem/SidebarItem.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './SidebarItem.css';
+
+
+class SidebarItem extends Component {
+  render() {
+    return (
+      <div className="SidebarItem">
+        <h2>SidebarItem</h2>
+      </div>
+    );
+  }
+}
+
+export default SidebarItem;
