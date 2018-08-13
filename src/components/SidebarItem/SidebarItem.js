19ea152ceec519313e4cae7196c8eccb07b8006e
Sheldon Woodward <sheldon.woodward@ahss.org>
2018-08-13 14:15:58 -0400

    Fixed timestamp time checking.

--- src/components/SidebarItem/SidebarItem.js
+++ src/components/SidebarItem/SidebarItem.js
@@ -7,14 +7,14 @@ class SidebarItem extends Component {
     let timestamp = new Date(this.props.note.timestamp);
     
     // within 24 hours
-    if (Date.now() - this.props.note.timestamp < 50400) {
+    if (Date.now() - this.props.note.timestamp < 50400000) {
       return timestamp.toLocaleTimeString("en-US", {
         hour: "2-digit",
         minute: "2-digit",
       });
     }
     // within a week
-    else if (Date.now() - this.props.note.timestamp < 352800) {
+    else if (Date.now() - this.props.note.timestamp < 352800000) {
       return timestamp.toLocaleDateString("en-US", {
         weekday: "long",
       });
