ee9f7b9a61cbd02d65633a9b577c4c6d540f1615
sheldonwoodward <sheldon.k.woodward@gmail.com>
2018-08-14 21:15:37 -0400

    Updated docs.

--- marknote/models.py
+++ marknote/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 
 class Note(models.Model):
-    id = models.CharField(primary_key=True)
-    title = models.CharField()
+    id = models.CharField(primary_key=True, max_length=36)
+    title = models.CharField(max_length=30)
     content = models.TextField()
-    folderId = models.CharField()
+    folderId = models.CharField(max_length=36)
     timestamp = models.IntegerField()
 
 
 class Folder(models.Model):
-    id = models.CharField(primary_key=True)
-    title = models.CharField()
-    folderId = models.CharField()
+    id = models.CharField(primary_key=True, max_length=36)
+    title = models.CharField(max_length=30)
+    folderId = models.CharField(max_length=36)
