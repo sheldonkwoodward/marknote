26d78db139516c35d0e8f12581c4e550d2253dc4
Sheldon Woodward <sheldon.woodward@ahss.org>
2018-08-17 09:25:48 -0400

    Updated models.

--- marknote/models.py
+++ marknote/models.py
@@ -5,15 +5,21 @@ class Base(models.Model):
     id = models.CharField(primary_key=True, max_length=36)
     title = models.CharField(max_length=30)
     containerId = models.ForeignKey('Folder', on_delete=models.CASCADE, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
+        ordering = ['title', '-updated']
 
 
 class Note(Base):
     content = models.TextField(null=True)
-    timestamp = models.IntegerField()
+
+    class Meta:
+        db_table = 'marknote_notes'
 
 
 class Folder(Base):
-    pass
+    class Meta:
+        db_table = 'marknote_folders'
