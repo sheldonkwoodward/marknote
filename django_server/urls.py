7390cf854ce0cdc93453a9dbe76a9ae4e674a8e1
sheldonwoodward <sheldon.k.woodward@gmail.com>
2018-08-18 09:01:51 -0400

    Fixed error in URI scheme.

--- django_server/urls.py
+++ django_server/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import include, url
+from django.urls import include, re_path
 from django.contrib import admin
 
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^/marknote/', include('marknote.urls', namespace='marknote'))
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^/marknote/', include(('marknote.urls', 'marknote'), namespace='marknote'))
 ]
