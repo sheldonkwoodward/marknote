ef6f0f2369a53e005b45a518b2944d7512c4e9cc
sheldonwoodward <sheldon.k.woodward@gmail.com>
2018-08-18 09:57:03 -0400

    Added custom id field.

--- marknote/models.py
+++ marknote/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Base(models.Model):
-    pk = models.CharField(primary_key=True, max_length=36)
+    id = models.CharField(primary_key=True, max_length=36)
     title = models.CharField(max_length=30)
     containerId = models.ForeignKey('Folder', on_delete=models.CASCADE, null=True)
     created = models.DateTimeField(auto_now_add=True)
