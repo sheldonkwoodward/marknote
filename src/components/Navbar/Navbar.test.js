280bbe2897890169e94d5b44ba23f12b6499c39f
sheldonwoodward <sheldon.k.woodward@gmail.com>
2018-09-06 16:07:43 -0400

    Removed Javascript stuff.

--- src/components/Navbar/Navbar.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Navbar from './Navbar';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Navbar />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
